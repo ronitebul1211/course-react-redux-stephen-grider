35082f7c2f8b87b03fdddcab414ec7ffff4ac4ff
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-17 13:33:06 +0200

    create songs app base files structure

---
+++ src/01-songs/components/SongsApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SongsApp = () => {
+   return (
+      <div className="songs-app">
+         <div>SongsApp</div>
+      </div>
+   );
+};
+
+export default SongsApp;
