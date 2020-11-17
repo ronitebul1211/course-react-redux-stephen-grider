7001d750a61b8ce8ec1868ac0a83c3b7a7e8d20d
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-17 15:21:50 +0200

    create store with app reducers and pass it to the provider wrap app component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 // App List
 import SongsApp from "./01-songs/components/SongsApp";
+import songsAppReducers from "./01-songs/reducers";
 
-ReactDOM.render(<SongsApp />, document.querySelector("#root"));
+ReactDOM.render(
+   <Provider store={createStore(songsAppReducers)}>
+      <SongsApp />
+   </Provider>,
+   document.querySelector("#root"),
+);
