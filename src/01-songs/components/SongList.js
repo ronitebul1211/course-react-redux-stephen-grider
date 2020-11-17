14cfd9913586907fcfca0fc388b97dd1c5ea0263
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-17 16:31:34 +0200

    get song list state in song list component as props via connect

--- src/01-songs/components/SongList.js
+++ src/01-songs/components/SongList.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class SongList extends Component {
    state = {};
    render() {
+      console.log(this.props);
       return <div>SongList</div>;
    }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+   return { songs: state.songs };
+};
+
+//connect function return function, SongList component passed in as arg to the function return from connect
+// first arg is rference to function get called with whole state and return object that will shows as props in component
+export default connect(mapStateToProps)(SongList);
