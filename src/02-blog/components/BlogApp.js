e7cc44a223e1bfe0ce94c0fddcee40d6312e388c
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-18 14:53:45 +0200

    init blog app

---
+++ src/02-blog/components/BlogApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogApp = () => {
+   return <div>Blog App</div>;
+};
+
+export default BlogApp;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-// App List
-import SongsApp from "./01-songs/components/SongsApp";
-import songsAppReducers from "./01-songs/reducers";
+// // 01 - Songs App
+// import SongsApp from "./01-songs/components/SongsApp";
+// import songsAppReducers from "./01-songs/reducers";
+// ReactDOM.render(
+//    <Provider store={createStore(songsAppReducers)}>
+//       <SongsApp />
+//    </Provider>,
+//    document.querySelector("#root"),
+// );
 
-ReactDOM.render(
-   <Provider store={createStore(songsAppReducers)}>
-      <SongsApp />
-   </Provider>,
-   document.querySelector("#root"),
-);
+// 01 - Blog App
+import BlogApp from "./02-blog/components/BlogApp";
+ReactDOM.render(<BlogApp />, document.querySelector("#root"));
