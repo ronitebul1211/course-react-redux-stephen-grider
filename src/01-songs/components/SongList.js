6e6b9f7b1b57be94b864c6586fa3811ad9bdaac0
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-18 12:46:13 +0200

    add the functionality to update selected song in state from song list component

--- src/01-songs/components/SongList.js
+++ src/01-songs/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
    renderList() {
@@ -7,7 +8,9 @@ class SongList extends Component {
          return (
             <div className="item" key={song.title}>
                <div className="right floated content">
-                  <button className="ui button primary">SELECT</button>
+                  <button className="ui button primary" onClick={() => this.props.selectSong(song)}>
+                     SELECT
+                  </button>
                </div>
 
                <div className="content">{song.title}</div>
@@ -24,6 +27,4 @@ const mapStateToProps = (state) => {
    return { songs: state.songs };
 };
 
-//connect function return function, SongList component passed in as arg to the function return from connect
-// first arg is rference to function get called with whole state and return object that will shows as props in component
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
