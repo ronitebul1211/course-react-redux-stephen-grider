3bb225b1854c1c88e980ee05935f1b91312008b1
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-22 08:17:29 +0200

    implement fetco post action without middleware - wrong way

--- src/02-blog/actions/index.js
+++ src/02-blog/actions/index.js
@@ -1,3 +1,5 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 /**
  * Action object : {
  *    type: "FETCH_POST"
@@ -5,8 +7,17 @@
  * }
  */
 
-export const fetchPosts = () => {
+export const fetchPosts = async () => {
+   const response = await jsonPlaceholder.get("/posts");
+   console.log(response);
    return {
       type: "FETCH_POST",
+      payload: response,
    };
 };
+
+/**
+ Breaking the rules of action creators
+ get error: Action must be plains objects. 
+            Use custom middleware for async actions
+ */
