70ff28ea08aaa8acc0f3ff6a5346ceb1e1e8489b
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-22 20:44:29 +0200

    refactor fetch posts and users action creator with lodash

--- src/02-blog/actions/index.js
+++ src/02-blog/actions/index.js
@@ -3,8 +3,13 @@ import _ from "lodash";
 
 export const fetchPostAndUsers = () => async (dispatch, getState) => {
    await dispatch(fetchPosts());
-   const userIds = _.uniq(_.map(getState().posts, "userId"));
-   userIds.forEach((id) => dispatch(fetchUser(id)));
+   // const userIds = _.uniq(_.map(getState().posts, "userId"));
+   // userIds.forEach((id) => dispatch(fetchUser(id)));
+   _.chain(getState().posts)
+      .map("userId")
+      .uniq()
+      .forEach((id) => dispatch(fetchUser(id)))
+      .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
